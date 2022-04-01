fe6aad2b735a5c815a203d2d1400252acf35513d
2021phpproject <jeemipatel1997@gmail.com>
2022-04-01 17:13:02 +0530

    test

--- test_LEAP.py
+++ test_LEAP.py
@@ -5,5 +5,5 @@ import unittest
 
 class TestEnum (unittest.TestCase):
     def test_nslookup(self):
-        self.assertEqual(LocalEnum.ns_lookup(
+        self.assertIsNone(LocalEnum.ns_lookup(
             self, "www.google.com"), "142.251.33.196")
